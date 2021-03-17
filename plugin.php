794653eade5d26c028df796a6ba9b8ced7a5fc1e
atiq-xs <atiqur.su@gmail.com>
2021-03-17 19:30:33 +0600

    #initial commit

--- plugin.php
+++ plugin.php
@@ -7,12 +7,13 @@ class Plugin {
 	public function init() {
 
 		$factory = new Provider_Factory();
-		$factory2 = new Factory2();
+		//$factory2 = new Factory2();
+
 		$social = $factory->get_object('facebook');
-		$social2 = $factory->get_object('google');
+		//$social2 = $factory->get_object('google');
 
 		echo 'User name : '. $social->get_name().'<br/>';
-		echo 'User name : '. $social2->get_name();
+		//echo 'User name : '. $social2->get_name();
 	}
 
 }
--- provider.php
+++ provider.php
@@ -7,4 +7,11 @@ interface Provider {
 	public function get_name();
 
 	public function get_user_id();
+
+	public function set_config() ;
+	public function auth() ;
+	public function get_token() ;
 }
+
+
+// facebook + github
